6d3604f28f4f9ee511c35e8894970d091cd3a6be
amisu <hamisurahman@gmail.com>
2023-11-03 15:06:27 +0000

    Register endpoint

---
+++ src/fakeDB.js
@@ -0,0 +1,7 @@
+exports.fakeDB = [
+    {
+        id: 0,
+        email: "hamisurahman@gmail.com",
+        password: "$2a$10$V65Ohq40xfqZTPL31WLk2eueDEjICxmwyRheOXmqyhyZrJUZquT7e" //test
+    }
+]
--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ const cookieParser = require('cookie-parser')
 const cors = require('cors');
 const {verify} = require('jsonwebtoken');
 const {hash, compare} = require('bcryptjs')
+const {fakeDB} = require('./fakeDB.js')
 
 
 const server = express()
 
 //Use express middleware for easier cookie handling
-server.use(cookieParser)
+server.use(cookieParser())
 
 server.use(
     cors({
@@ -26,14 +27,31 @@ server.listen(process.env.PORT, () => {
     console.log(`Server listening on port ${process.env.PORT}`)
 })
 
+server.get('/', (req, res) => {
+    console.log('hmaisu')
+    res.status(200).send("<h1>Welcome<h1/>")
+})
 
 // 1. Register a user
 server.post('/register', async (req, res) => {
     const {email, password} = req.body;
     try {
         // 1. check if user exist
-
+        const user = fakeDB .find(user => user.email === email)
+        if (user) throw new Error('User already exist')
+        //if not user exist, hash the password
+        const hashedPassword = await hash(password, 10)
+        //Insert the user in 'database'
+        fakeDB.push({
+            id:fakeDB.length,
+            email,
+            password: hashedPassword
+        })
+        res.send({message: 'user created'})
+        console.log(fakeDB);
     } catch(err) {
-
+        res.send({
+            error: `${err.message}`
+        })
     }
 })
